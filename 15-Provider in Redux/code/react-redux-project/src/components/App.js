0d65b200c951ea818e67b3bb0c3da5ced991c9cb
Nguyen Duc Hoang <sunlight4d@gmail.com>
2017-09-10 16:28:39 +0700

    update

---
+++ 15-Provider in Redux/code/react-redux-project/src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+            <h2>Welcome to React with Redux project</h2>
+        </div>
+        <p className="App-intro">
+          <p>This is a tutorial React app with Redux</p>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
